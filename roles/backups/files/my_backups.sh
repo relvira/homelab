e4e039845cc5a3e91502d2ef14dbcf69ef275a1f
Rafael Elvira <relviratellez@gmail.com>
2021-01-17 20:25:51 +0000

    backups script cleanup

--- roles/backups/files/my_backups.sh
+++ roles/backups/files/my_backups.sh
@@ -10,28 +10,31 @@ echo "---  Starting backups at $(date)  ---"
 echo "-------------------------------------"
 echo " "
 
+# Set GPG things
+export GPG_TTY=$(tty)
+GPG_OPTS='--pinentry-mode loopback'
+
 # Enable Nextcloud maintenance mode while backups
 echo "+ Enabling Nextcloud maintenance mode for data and mysql backups..."
 docker exec --user www-data nextcloud php occ maintenance:mode --on
 
-echo "+ Sleeping 30 seconds until backups start..."
-sleep 30
+echo "+ Sleeping 5 seconds until backups start..."
+sleep 5
 
 ### Start Nextcloud data ###
-echo "+ Running Duplicity for $NEXTCLOUD_DATA_SOURCE_PATH "
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity --use-agent \
+echo "+ Running Duplicity for $NEXTCLOUD_DATA_SOURCE_PATH"
+duplicity \
         --verbosity notice \
-        --encrypt-key "$BACKUP_GPG_ENC_KEY" \
         --full-if-older-than 13D \
         --num-retries 3 \
         --archive-dir /root/.cache/duplicity \
         --volsize 200 \
       "$NEXTCLOUD_DATA_SOURCE_PATH" rsync://ret@anton.mad.home/"$NEXTCLOUD_DATA_DEST_PATH"
 
-# Cleanup
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity cleanup --extra-clean --force rsync://ret@anton.mad.home/"$NEXTCLOUD_DATA_DEST_PATH"
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity remove-all-inc-of-but-n-full 2 --force rsync://ret@anton.mad.home/"$NEXTCLOUD_DATA_DEST_PATH"
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity remove-all-but-n-full 2 --force rsync://ret@anton.mad.home/"$NEXTCLOUD_DATA_DEST_PATH"
+# Cleanup nextcloud data backups
+duplicity cleanup --extra-clean --force rsync://ret@anton.mad.home/"$NEXTCLOUD_DATA_DEST_PATH"
+duplicity remove-all-inc-of-but-n-full 2 --force rsync://ret@anton.mad.home/"$NEXTCLOUD_DATA_DEST_PATH"
+duplicity remove-all-but-n-full 2 --force rsync://ret@anton.mad.home/"$NEXTCLOUD_DATA_DEST_PATH"
 
 echo "+ Duplicity completed for $NEXTCLOUD_DATA_SOURCE_PATH "
 ### End Nextcloud data ###
@@ -45,19 +48,18 @@ mysqldump --single-transaction \
 echo "+ mysqldump completed"
 
 echo "+ Running Duplicity for $MYSQL_SOURCE_PATH "
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity --use-agent \
+duplicity \
         --verbosity notice \
-        --encrypt-key "$BACKUP_GPG_ENC_KEY" \
         --full-if-older-than 13D \
         --num-retries 3 \
         --archive-dir /root/.cache/duplicity \
         --volsize 200 \
       "$MYSQL_SOURCE_PATH" rsync://ret@anton.mad.home/"$MYSQL_DEST_PATH"
 
-# Cleanup
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity cleanup --extra-clean --force rsync://ret@anton.mad.home/"$MYSQL_DEST_PATH"
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity remove-all-inc-of-but-n-full 2 --force rsync://ret@anton.mad.home/"$MYSQL_DEST_PATH"
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity remove-all-but-n-full 2 --force rsync://ret@anton.mad.home/"$MYSQL_DEST_PATH"
+# Cleanup MySQL backups
+duplicity cleanup --extra-clean --force rsync://ret@anton.mad.home/"$MYSQL_DEST_PATH"
+duplicity remove-all-inc-of-but-n-full 2 --force rsync://ret@anton.mad.home/"$MYSQL_DEST_PATH"
+duplicity remove-all-but-n-full 2 --force rsync://ret@anton.mad.home/"$MYSQL_DEST_PATH"
 
 echo "+ Duplicity completed for $MYSQL_SOURCE_PATH "
 ### End MySQL ###
@@ -67,54 +69,51 @@ echo "+ Disabling Nextcloud maintenance mode..."
 docker exec --user www-data nextcloud php occ maintenance:mode --off
 
 echo "+ Running Duplicity for $ETC_SOURCE_PATH "
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity --use-agent \
+duplicity \
         --verbosity notice \
-        --encrypt-key "$BACKUP_GPG_ENC_KEY" \
         --full-if-older-than 13D \
         --num-retries 3 \
         --archive-dir /root/.cache/duplicity \
         --volsize 200 \
       "$ETC_SOURCE_PATH" rsync://ret@anton.mad.home/"$ETC_DEST_PATH"
 
-# Cleanup
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity cleanup --extra-clean --force rsync://ret@anton.mad.home/"$ETC_DEST_PATH"
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity remove-all-inc-of-but-n-full 2 --force rsync://ret@anton.mad.home/"$ETC_DEST_PATH"
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity remove-all-but-n-full 2 --force rsync://ret@anton.mad.home/"$ETC_DEST_PATH"
+# Cleanup /etc backups
+duplicity cleanup --extra-clean --force rsync://ret@anton.mad.home/"$ETC_DEST_PATH"
+duplicity remove-all-inc-of-but-n-full 2 --force rsync://ret@anton.mad.home/"$ETC_DEST_PATH"
+duplicity remove-all-but-n-full 2 --force rsync://ret@anton.mad.home/"$ETC_DEST_PATH"
 
 ### Start of www backup ###
 echo "+ Running Duplicity for $WWW_SOURCE_PATH "
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity --use-agent \
+duplicity \
         --verbosity notice \
-        --encrypt-key "$BACKUP_GPG_ENC_KEY" \
         --full-if-older-than 13D \
         --num-retries 3 \
         --archive-dir /root/.cache/duplicity \
         --volsize 200 \
       "$ETC_SOURCE_PATH" rsync://ret@anton.mad.home/"$WWW_DEST_PATH"
 
-# Cleanup
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity cleanup --extra-clean --force rsync://ret@anton.mad.home/"$WWW_DEST_PATH"
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity remove-all-inc-of-but-n-full 2 --force rsync://ret@anton.mad.home/"$WWW_DEST_PATH"
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity remove-all-but-n-full 2 --force rsync://ret@anton.mad.home/"$WWW_DEST_PATH"
+# Cleanup www backups
+duplicity cleanup --extra-clean --force rsync://ret@anton.mad.home/"$WWW_DEST_PATH"
+duplicity remove-all-inc-of-but-n-full 2 --force rsync://ret@anton.mad.home/"$WWW_DEST_PATH"
+duplicity remove-all-but-n-full 2 --force rsync://ret@anton.mad.home/"$WWW_DEST_PATH"
 
 echo "+ Duplicity completed for $WWW_SOURCE_PATH "
 ### End of www backup ###
 
 ### Start of Plex library backup ###
 echo "+ Running Duplicity for $PLEX_LIB_SOURCE_PATH "
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity --use-agent \
+duplicity \
         --verbosity notice \
-        --encrypt-key "$BACKUP_GPG_ENC_KEY" \
         --full-if-older-than 13D \
         --num-retries 3 \
         --archive-dir /root/.cache/duplicity \
         --volsize 200 \
       "$ETC_SOURCE_PATH" rsync://ret@anton.mad.home/"$PLEX_LIB_DEST_PATH"
 
-# Cleanup
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity cleanup --extra-clean --force rsync://ret@anton.mad.home/"$PLEX_LIB_DEST_PATH"
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity remove-all-inc-of-but-n-full 2 --force rsync://ret@anton.mad.home/"$PLEX_LIB_DEST_PATH"
-PASSPHRASE="$BACKUP_GPG_PASSPHRASE" duplicity remove-all-but-n-full 2 --force rsync://ret@anton.mad.home/"$PLEX_LIB_DEST_PATH"
+# Cleanup plex backups
+duplicity cleanup --extra-clean --force rsync://ret@anton.mad.home/"$PLEX_LIB_DEST_PATH"
+duplicity remove-all-inc-of-but-n-full 2 --force rsync://ret@anton.mad.home/"$PLEX_LIB_DEST_PATH"
+duplicity remove-all-but-n-full 2 --force rsync://ret@anton.mad.home/"$PLEX_LIB_DEST_PATH"
 
 echo "+ Duplicity completed for $PLEX_LIB_SOURCE_PATH "
 ### End of Plex library backup ###
